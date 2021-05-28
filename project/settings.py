8ea3dfea9a9ccef5c45afa35de0a760843ba6b80
Farid-mo <Farid-mo>
2021-05-28 12:28:44 +0100

    set debug to true

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'uwv2geu0*b6lya!p=mtdxnaxp*whn%v6h51q!hnpb)ug%8of2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
