dc9491193b9ee250cca6b7e6ceb2fa4f882c67f7
Farid-mo <Farid-mo>
2021-05-29 17:39:22 +0100

    forms editing

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Product
+
+
+class ProductForm(UserCreationForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'image', 'user_id', 'tags_id', 'date_created']
+
+        def __str__(self):
+            return self.name
+
+
+
+
+
