762542ca0757203c077ca6029e3882e99804d793
Farid-mo <faridmouzoune@outlook.fr>
2021-06-02 12:03:19 +0100

    edit user registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -10,6 +10,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     add = models.CharField(max_length=50, blank=True, null=True)
+    # pic_profile = models.ImageField(upload_to='accounts_images/', null=True)
+    # height_field=None, width_field=None, max_length=100, **options
 
     def __str__(self):
         return str(self.user)
@@ -25,7 +27,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 class Friend(models.Model):
-    friend = models.ForeignKey(User, null=True,on_delete=models.SET_NULL)
+    friend = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name_plural = 'Friends'
--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Profile
-from .forms import SignupForm
+from .forms import SignupForm, ProfileForm
 from django.contrib.auth import authenticate, login
+from .models import Profile
 # Create your views here.
 
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-        if form.is_valid():
+        p_form = ProfileForm(request.POST)
+        if form.is_valid() and p_form.is_valid():
             form.save()
+            p_form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('/accounts/profile')
-
+            if user is not None:
+                login(request, user)
+                return redirect('/accounts/profile')
     else:
         form = SignupForm()
-    return render(request, 'registration/signup.html', {'forms': form})
+        p_form = ProfileForm
+    return render(request, 'registration/signup.html', {'form': form, 'p_form': p_form})
 
 
 def profile(request):
